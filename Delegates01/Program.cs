6e7f43c3040e8e4c96f1b1371cd1431177e7b806
Aydın Türker <doktoralban@outlook.com>
2021-07-19 20:10:12 +0300

    Add project files.

---
+++ Delegates01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegates01
+{
+    // Delegate Definition  
+    public delegate int operation(int x, int y);
+
+    class Program
+    {
+        // Method that is passes as an Argument  
+        // It has same signature as Delegates   
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            // Delegate instantiation  
+            operation oper = new operation(Program.Addition);
+
+            // output  
+            Console.WriteLine("Addition is={0}", oper(15, 42));
+            Console.ReadLine();
+        }
+
+
+    }
+}
